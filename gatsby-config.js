4bc77952930b12d87f686811dc61bc8364563293
kotou@actynetwork <maikotou@MainoiMac.local>
2023-03-04 12:26:07 +0900

    pakage,configを導入

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,71 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
+const gatsbyRemarkImages = require('gatsby-remark-images');
+
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: `/app`,
+
   siteMetadata: {
-    title: `gatsby portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `M.K`,
+    description: `2020年から2023年までの制作実績サイトです。`,
+    author: `Mai Kotou`,
+    user: { name: 'M.kotou', email: 'm.kotou0715@gmail.com' },
   },
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
     },
-    __key: "pages"
-  }]
+    {
+      resolve: `gatsby-plugin-web-font-loader`,
+      options: {
+        typekit: {
+          id: process.env.GATSBY_ADOBE_FONT_PROJECT_ID,
+        },
+      },
+    },
+    `gatsby-plugin-transition-link`,
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-remark-images`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`,`.md`],
+          gatsbyRemarkPlugins: [
+          {
+              resolve: `gatsby-remark-images`,
+              options: {
+                maxWidth: 680,
+                linkImagesToOriginal: true,
+              },
+            },
+        ],
+      },
+    },
+    {
+    resolve: `gatsby-plugin-manifest`,
+    options:{
+      icon: `./src/images/powerGirl.png`,
+      }
+    },
+    `gatsby-plugin-emotion`,
+  ],
 };
